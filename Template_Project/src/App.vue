01c43e9dc1d97084a8447a167fbed0f52338db80
Leo <leonard.sangra@students.salle.url.edu>
2022-10-17 14:18:40 +0200

    RegisterView UP

--- Template_Project/src/App.vue
+++ Template_Project/src/App.vue
@@ -7,7 +7,7 @@ import { RouterLink, RouterView } from "vue-router";
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/about">Register</RouterLink>
+        <RouterLink to="/register">Register</RouterLink>
       </nav>
   </header>
   <main>
