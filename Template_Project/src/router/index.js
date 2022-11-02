d6690eff2a4b1c79003221741c9b7aad6d4d64cd
kevineljarratohayon <kevineljarrat@gmail.com>
2022-11-03 00:29:27 +0100

    Ya está hecha la vista, faltan un par de retoques para acabar el diseño.

--- Template_Project/src/router/index.js
+++ Template_Project/src/router/index.js
@@ -18,6 +18,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
     },
+    {
+      path: "/addfriend",
+      name: "addfriend",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/AgregarAmigo.vue"),
+    },
     /*
     {
       path: "/contact",
---
+++ Template_Project/src/views/AgregarAmigo.vue
@@ -0,0 +1,27 @@
+<template>
+  <link rel="stylesheet" href="src/assets/styles/styleAgregarAmigo.css" />
+
+  <section class="body">
+    <section class="seccionIzquierda">
+      <section class="searchBar">
+        <section><h2>Find your next event companion</h2></section>
+        <section class="box">
+          <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+          <i class="fa fa-search" aria-hidden="true"></i>
+          <input type="text" placeholder="Username" />
+        </section>
+      </section>
+    </section>
+    <section class="seccionDerecha">
+      <img src="src/assets/images/imagenAmigos.jpeg" alt="" width="480" />
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AgregarAmigo",
+};
+</script>
+
+<style scoped></style>
