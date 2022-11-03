e17aba77452ab6477a359f62bb68ad328ccbc815
albamayol <alba.mayol@students.salle.url.edu>
2022-11-03 17:45:28 +0100

    Canvis estructura HTML i canvis inicials pel css d'amigos

--- Template_Project/src/views/AmigosView.vue
+++ Template_Project/src/views/AmigosView.vue
@@ -1,4 +1,5 @@
 <template>
+  <Link rel="stylesheet" href="src/assets/styles/amigos.css"/>
   <!-- cuando se entre a esta view (botón) se hará una petición a la API -> name attribute
   id is used to uniquely identify an HTML element. Ideally, an id should only identify one (and only one) HTML element across your entire document.
 
@@ -6,32 +7,37 @@
 
   name is (mostly) used to identify elements in a form.
   -->
-  <section>
-    <div style="background: blueviolet; ">
-      <h2 style="background: blue">Friends: </h2>
-      <div class="searchBox" style="background: darkgoldenrod">
-        <input type="text">
-      </div>
-    </div>
-  </section>
-  <section style="background: coral">
-    <nav style="background: crimson">
-      <ul id="friendsList">
-        <li>Link 1</li>
-        <li>Link 2</li>
-        <li>Link 3</li>
-        <li>Link 4</li>
-        <li>Link 5</li>
-        <li>Link 6</li>
-        <li>Link 7</li>
-        <li>Link 8</li>
-        <li>Link 9</li>
-        <li>Link 10</li>
-        <li>Link 11</li>
-        <li>Link 13</li>
-        <li>Link 13</li>
-      </ul>
-    </nav>
+  <section class="body">
+    <section style="background: darkslateblue" class="top">
+      <section class="search">
+        <h2 style="background: darkcyan">Friends: </h2>
+        <div class="searchBox" style="background: darkgoldenrod">
+          <input type="text">
+        </div>
+      </section>
+        <div class="buttonDiv">
+          <button type="button">Add friend</button>
+        </div>
+    </section>
+
+    <section style="background: coral" class="recyclerView">
+        <!--<ul id="friendsList">-->
+        <ul class="list">
+          <li>Link 1</li>
+          <li>Link 2</li>
+          <li>Link 3</li>
+          <li>Link 4</li>
+          <li>Link 5</li>
+          <li>Link 6</li>
+          <li>Link 7</li>
+          <li>Link 8</li>
+          <li>Link 9</li>
+          <li>Link 10</li>
+          <li>Link 11</li>
+          <li>Link 13</li>
+          <li>Link 13</li>
+        </ul>
+    </section>
   </section>
 </template>
 
