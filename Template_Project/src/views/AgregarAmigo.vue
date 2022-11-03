057c14d1979a5cffd6b35ab6e863254c99fa739f
kevineljarratohayon <kevineljarrat@gmail.com>
2022-11-03 15:21:14 +0100

    He aplicado algunos retoques al icono de la search bar así como cambiado el estilo de la letra al título.

--- Template_Project/src/views/AgregarAmigo.vue
+++ Template_Project/src/views/AgregarAmigo.vue
@@ -3,11 +3,11 @@
 
   <section class="body">
     <section class="seccionIzquierda">
-      <section class="searchBar">
+      <section class="box">
         <section><h2>Find your next event companion</h2></section>
-        <section class="box">
+        <section class="searchBar">
           <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-          <i class="fa fa-search" aria-hidden="true"></i>
+          <i style="font-size:16px;color:grey" class="fa fa-search" aria-hidden="true"></i>
           <input type="text" placeholder="Username" />
         </section>
       </section>
