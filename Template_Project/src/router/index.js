b0ff9813dcb9d1731bd456572899edcee857e8e6
kevineljarratohayon <kevineljarrat@gmail.com>
2022-11-12 10:47:16 +0100

    Pequeño error de indexación con el server

--- Template_Project/src/router/index.js
+++ Template_Project/src/router/index.js
@@ -19,12 +19,12 @@ const router = createRouter({
       component: () => import("../views/AboutView.vue"),
     },
     {
-      path: "/userHome",
-      name: "userHome",
+      path: "/usuarioHome",
+      name: "usuarioHome",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/userHome.vue"),
+      component: () => import("../views/usuarioHome.vue"),
     }
     /*
     {
