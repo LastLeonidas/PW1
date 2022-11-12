c87134e41cf2661d53cf3544b051ac8f8bf75d48
kevineljarratohayon <kevineljarrat@gmail.com>
2022-11-12 19:18:35 +0100

    He hecho los últimos ajuestes

--- Template_Project/src/views/UsuarioHome.vue
+++ Template_Project/src/views/UsuarioHome.vue
@@ -4,35 +4,27 @@
   <section class="body">
     <section class="arriba">
       <section class="opcionesArriba">
-        <img src="src/assets/images/charla.png" width="150px" alt="Logo Charla">
-        <h2>Chat</h2>
+        <img class="imagenesIconos" src="../assets/media/imagenes/charla.png" alt="Logo Charla">
+        <h2 class="nombreSecciones">Chat</h2>
       </section>
       <section class="opcionesArriba">
-        <img src="src/assets/images/usuario-de-perfil.png" width="150px" alt="Logo Perfil Usuario">
-        <h2>Profile</h2>
+        <img class="imagenesIconos" src="../assets/media/imagenes/usuario-de-perfil.png" alt="Logo Perfil Usuario">
+        <h2 class="nombreSecciones">Profile</h2>
       </section>
       <section class="opcionesArriba">
-        <img src="src/assets/images/amigos.png" width="150px" alt="Logo Amigos">
-        <h2>Friends</h2>
+        <img class="imagenesIconos" src="../assets/media/imagenes/amigos.png" alt="Logo Amigos">
+        <h2 class="nombreSecciones">Friends</h2>
       </section>
     </section>
     <section class="abajo">
       <section class="opcionesAbajo">
-        <img src="src/assets/images/cerrar-sesion.png" width="150px" alt="Logo Cerrar Sesion">
-        <h2>Close Session</h2>
+        <img class="imagenesIconos" src="../assets/media/imagenes/cerrar-sesion.png" alt="Logo Cerrar Sesion">
+        <h2 class="nombreSecciones">Close Session</h2>
       </section>
       <section class="opcionesAbajo">
-        <img src="src/assets/images/eliminar-cuenta.png" width="150px" alt="Logo Eliminar Cuenta">
-        <h2>Delete Account</h2>
+        <img class="imagenesIconos" src="../assets/media/imagenes/eliminar-cuenta.png" alt="Logo Eliminar Cuenta">
+        <h2 class="nombreSecciones">Delete Account</h2>
       </section>
     </section>
   </section>
 </template>
-
-<script>
-export default {
-  name: "UsuarioHome",
-};
-</script>
-
-<style scoped></style>
