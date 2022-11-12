cbebbf5aa60dea438af2e9b7277212dc695fb8e2
Sandra Corral Navarro <sandra.corral@students.salle.url.edu>
2022-11-12 11:50:27 +0100

    Está todo. Los únicos cambios que habría que hacer es cambiar el css para que haga lo del móvil.

---
+++ Template_Project/src/views/EventsList.vue
@@ -0,0 +1,50 @@
+<template>
+  <main>
+    <header>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Profile</title>
+      <link rel="stylesheet" href="src/assets/styleEventsList.css">
+    </header>
+    <body>
+    <div class="container">
+      <div class="event">
+        <img src="https://highxtar.com/wp-content/uploads/2022/02/thumb-dua-lipa-balenciaga.jpg" alt="">
+        <div class="infoEvent">
+          <h2>Dua Lipa Concert</h2>
+          <p>Place: Palau Sant Jordi</p>
+          <p>Dates: Tuesday 4th October</p>
+        </div>
+        <button>Enter</button>
+      </div>
+      <div class="event">
+        <img src="https://highxtar.com/wp-content/uploads/2022/02/thumb-dua-lipa-balenciaga.jpg" alt="">
+        <div class="infoEvent">
+          <h2>Dua Lipa Concert</h2>
+          <p>Place: Palau Sant Jordi</p>
+          <p>Dates: Tuesday 4th October</p>
+        </div>
+        <button>Enter</button>
+      </div>
+      <div class="event">
+        <img src="https://highxtar.com/wp-content/uploads/2022/02/thumb-dua-lipa-balenciaga.jpg" alt="">
+        <div class="infoEvent">
+          <h2>Dua Lipa Concert</h2>
+          <p>Place: Palau Sant Jordi</p>
+          <p>Dates: Tuesday 4th October</p>
+        </div>
+        <button>Enter</button>
+      </div>
+    </div>
+    <!-- -->
+    </body>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "EventsList.vue"
+}
+</script>
+
+<style scoped>
+</style>
