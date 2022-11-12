d519ae0c1f56ddbf68e83a4521a7059525df35b5
Leo <leonard.sangra@students.salle.url.edu>
2022-11-12 14:29:58 +0100

    Fixed Again Error Navbar

--- Template_Project/src/router/index.js
+++ Template_Project/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import RegisterView from "../views/RegisterView.vue";
-import EventsView from "../views/EventsView.vue";
+//import EventsView from "../views/EventsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,7 +27,7 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: RegisterView,
-    },
+    },/*
     {
       path: "/events",
       name: "events",
